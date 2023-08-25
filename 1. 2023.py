62fedf10ef50f2a865df922faabef38fa14ee22a
Arda Aksu <79632956+aarda55@users.noreply.github.com>
2023-08-25 22:10:08 +0200

    Add files via upload

---
+++ 1. 2023.py
@@ -0,0 +1,18 @@
+def mr(number):
+    number_str = str(number)
+    replaced_str = number_str.replace('0', '1')
+    result = 1
+    for digit_str in replaced_str:
+        digit = int(digit_str)
+        result *= digit
+
+    return result
+
+p=[]
+
+for i in range(1000,9999):
+    i_with1 = mr(i)
+    if(i_with1 == 12):
+        p.append(i)
+
+print(len(p))
